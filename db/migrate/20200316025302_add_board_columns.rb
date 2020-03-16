f7de423d61b9d03c4cb3ab886a5750bd7dd89629
jeejon <jeejon@stunitas.com>
2020-03-16 23:45:05 +0900

    board에 image 추가

--- db/migrate/20200316025302_add_board_columns.rb
+++ db/migrate/20200316025302_add_board_columns.rb
@@ -1,5 +1,5 @@
-class AddAuditColumns < ActiveRecord::Migration[6.0]
+class AddBoardColumns < ActiveRecord::Migration[6.0]
   def change
-    add_column :boards, :image, :string, null: true, limit: 1000, default: ''
+    add_column :boards, :image, :string, null: true, limit: 1000
   end
 end
