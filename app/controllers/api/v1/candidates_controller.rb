0c95cedc61b3d6d204212741073e0473d5574421
Hyungsung Shim <hyungsungshim@Hyungsungui-MacBookPro.local>
2020-03-29 13:23:44 +0900

    number * 1

--- app/controllers/api/v1/candidates_controller.rb
+++ app/controllers/api/v1/candidates_controller.rb
@@ -19,7 +19,7 @@ module Api
         @candidates = @district.present?? Candidate.where(voting_district: @district) : Candidate.all
         # @candidates = @candidates.where(crawl_id: candidate&.crawl_id)
         # @candidates = @candidates.order(number: :asc)
-        @candidates = @candidates.order('number+0 asc')
+        @candidates = @candidates.order('number*1 asc')
         @candidates = @candidates.page(params[:page]).per(params[:per_page])
         @meta = get_page_info(@candidates).merge(meta_status)
       end
